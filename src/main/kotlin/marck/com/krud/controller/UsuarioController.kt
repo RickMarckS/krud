ab4b23021a45ce36a34378e5d2e8826e453ed84f
Rick Marck <rickmarck7@gmail.com>
2025-03-18 14:56:17 -0400

    :sparkles: feat: adds POST endpoint to create new user

--- src/main/kotlin/marck/com/krud/controller/UsuarioController.kt
+++ src/main/kotlin/marck/com/krud/controller/UsuarioController.kt
@@ -1,15 +1,41 @@
 package marck.com.krud.controller
 
+import jakarta.validation.Valid
+import marck.com.krud.model.Usuario
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class UsuarioController {
 
 
     @GetMapping("/formulario/cadastro")
-    fun abrirFormularioCadastro(): String{
+    fun abrirFormularioCadastro(model: Model): String{
+
+        val usuario: Usuario = Usuario()
+        model.addAttribute("usuario", usuario)
 
         return "formulario-cadastro"
     }
+
+    @PostMapping("/cadastrar")
+    fun cadastrarUsuario(@Validated usuario: Usuario, result: BindingResult): String{
+
+        if (result.hasErrors()){
+            return "formulario-cadastro"
+        }
+
+        //salvar no bd
+        println(usuario)
+
+        return "home"
+    }
+
+
+
 }
